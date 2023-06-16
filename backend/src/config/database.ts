5c7f24bb7aa5b22fef3b0c52896bc18c840f2435
James Nicholson <jamesnicholson@Jamess-MacBook-Pro.local>
2023-06-16 19:54:22 +0200

    conf update

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 export const database = new Sequelize('myDatabase', 'root', 'jim', {
     host: '127.0.0.1',
-    port: 3306,
+    port: 3307,
     dialect: 'mysql',
 });
 
