bcfded4881f92ea3d48b12c757e497ac4d27ba9f
James Nicholson <jamesnicholson@Jamess-MacBook-Pro.local>
2024-03-07 20:54:44 +0200

    Some meore updates with eslint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import { ApolloServer } from 'apollo-server';
 import { buildSchema } from 'type-graphql';
 import { ProductResolver } from './resolvers/Product';
 import { database } from './config/database';
+import { goldbox } from './services/gb'
 async function main() {
 
     const schema = await buildSchema({
@@ -16,9 +17,13 @@ async function main() {
     });
     database.sync({ force: true })
         .then(() => {
+
+            goldbox();
             console.log(`Database & tables created!`)
+
         });
     await server.listen(4000);
     console.log("Server has started!");
+
 }
 main().catch((err) => console.error(err));
--- backend/src/services/gb.ts
+++ backend/src/services/gb.ts
@@ -1,93 +1,23 @@
-
-import puppeteer from 'puppeteer'
+import puppeteer from 'puppeteer';
 import { upsertProduct } from '../models/Product';
 
-async function goldbox() {
-    const amazon_url = "https://www.amazon.com/gp/goldbox";
-    try {
-        // Launch the headless browser 
-        const browser = await puppeteer.launch();
-        const basePage = await browser.newPage();
-        // Go to the webpage 
-        await basePage.goto(amazon_url, { timeout: 0 });
-        await basePage.waitForSelector('[data-testid="deal-card"]');
-        // Perform a function within the given webpage context 
-        const data = await basePage.evaluate(() => {
-            const results: any[] = [];
-            const items = document.querySelectorAll('[data-testid="deal-card"]');
-            items.forEach(item => {
-                const item_ = {
-                    name: "hello",
-                    dealId: item?.getAttribute("data-deal-id"),
-                    url: item?.querySelector("a.a-size-mini.a-link-normal")?.getAttribute("href"),
-                    shortDescription: item?.querySelector("a.a-size-mini.a-link-normal")?.getAttribute("title"),
-                    image: item?.querySelector("img")?.getAttribute("src")
-                }
-                results.push(item_);
-            });
+export async function goldbox() {
+  console.log('Goldbox');
+  const selector = '[data-testid="grid-deals-container"]';
+  const selectorImg = '[data-testid="grid-deals-container"]';
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://www.amazon.com/gp/goldbox/');
 
-            return results;
-        });
-        const [page] = await browser.pages();
-        const pages = [];
-
-        for (let i = 0; i < data.length; i++) {
-            const subPageURL = data[i].url;
-            await page.goto(`${subPageURL}`, {
-                waitUntil: "domcontentloaded"
-            });
-
-            pages.push({
-                title: await page.evaluate(() => document.querySelector(".a-container h1") && document.querySelector(".a-container h1")?.textContent?.toString()),
-                products: await page.evaluate(() => {
-                    const _products = [];
-                    const _items = document.querySelectorAll(".a-container .a-list-item");
-                    const productPageTitle = document.querySelector("#productTitle");
-                  if (productPageTitle === null) {
-                         _items.forEach((item) => {
-                            const img = item.querySelector("img") && item.querySelector("img")?.getAttribute("src");
-                            const url = item.querySelector(".a-link-normal") && item.querySelector(".a-link-normal")?.getAttribute("href");
-                            const title = item.querySelector(".a-link-normal") && item.querySelector(".a-link-normal")?.getAttribute("title");
-                            const price = item.querySelector(".a-price span") && item.querySelector(".a-price span")?.innerHTML;
-                            const oldPrice = item.querySelector(".octopus-widget-strike-through-price") !== null && item.querySelector("span.octopus-widget-strike-through-price")?.textContent
-                            const id =  item?.getAttribute("data-deal-id") !== null && item?.getAttribute("data-deal-id")
-                                _products.push({
-                                    img: `${img}`,
-                                    url: `${url}`,
-                                    title: `${title}`,
-                                    price: `${price}`,
-                                    oldPrice: `${oldPrice}`,
-                                    productId:`${id}`,
-                                });
-                            
-                        })
-                    } else {
-                        const productPageImg = document.querySelector("#imgTagWrapperId img")?.getAttribute("src");
-                        const productPageApexPriceToPay = document.querySelector(".apexPriceToPay .a-offscreen") && document.querySelector(".apexPriceToPay .a-offscreen")?.textContent;
-                        const productPageListedPrice = document.querySelector(".a-price.a-text-price .a-offscreen") && document.querySelector(".a-price.a-text-price .a-offscreen")?.textContent;  
-                        _products.push({
-                                img: `${productPageImg}`,
-                                url: `${document.URL}`,
-                                title: `${productPageTitle.textContent}`,
-                                price: `${productPageApexPriceToPay}`,
-                                oldPrice:`${productPageListedPrice}`,
-                                productId: '',
-                            });
-                    }
-                    return _products
-                }),
-            })
-        }
-        await browser.close();
-        for (let products of pages) {
-            for( let product of products.products){
-             await upsertProduct(product);
-            }
-        }
-      
-    } catch (error) {
-        console.error(error);
-    }
+  const deals = await page.waitForSelector('div.a-image-container img');
+  const imagefiles = await page.$$eval('.a-link-normal', (el) =>
+    el
+      .filter((xx) => xx.querySelector('.a-image-container img') !== null)
+      .map((a) => ({
+        link: a.getAttribute('href'),
+        img: a.querySelector('.a-image-container img').getAttribute('src'),
+      }))
+  );
+  console.log(imagefiles);
+  await browser.close();
 }
-
-goldbox();
